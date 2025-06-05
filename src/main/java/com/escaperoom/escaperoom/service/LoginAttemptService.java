1c95a6459d9d6b24d0070f8c86b84fa6744399f2
WilliamNyacka <williamnyacka14@gmail.com>
2025-06-05 23:22:06 +0200

    new

--- src/main/java/com/escaperoom/escaperoom/service/LoginAttemptService.java
+++ src/main/java/com/escaperoom/escaperoom/service/LoginAttemptService.java
@@ -14,91 +14,87 @@ import java.util.concurrent.TimeUnit;
 
 @Service
 public class LoginAttemptService {
-    private static final int MAXIMUM_NUMBER_OF_ATTEMPTS =5;
-    private static final int ATTEMPT_INCREMENT =1;
-    private LoadingCache<String,Integer> loginAttemptCache;
-    
-    @Autowired
-    private HttpServletRequest request;
-    
+	private static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
+	private static final int ATTEMPT_INCREMENT = 1;
+	private LoadingCache<String, Integer> loginAttemptCache;
 
-    public LoginAttemptService() {
-        super();
-        loginAttemptCache = 
-        		CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.DAYS)
-                .build(
-                        new CacheLoader<String, Integer>() {
-                            @Override
-                            public Integer load(final String key) {
-                                return 0;
-                            }
-                        }
-                );
-    }
+	@Autowired
+	private HttpServletRequest request;
 
-    public void addUserToLoginAttemptCache(String username) {
-        int attempts =0;
+	public LoginAttemptService() {
+		super();
+		loginAttemptCache = CacheBuilder.newBuilder().expireAfterWrite(1, TimeUnit.DAYS)
+				.build(new CacheLoader<String, Integer>() {
+					@Override
+					public Integer load(final String key) {
+						return 0;
+					}
+				});
+	}
 
-        try {
-            attempts = ATTEMPT_INCREMENT +loginAttemptCache.get(username);
+	public void addUserToLoginAttemptCache(String username) {
+		int attempts = 0;
 
-        } catch (ExecutionException e) {
-           e.printStackTrace();
-        }
-        loginAttemptCache.put(username,attempts);
+		try {
+			attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
 
-    }
-    public boolean hasExceededMaxAttempts(String username){
-        try {
-            return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;  //Echec de connexion
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        return false;   //Connexion OK
-    }
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		loginAttemptCache.put(username, attempts);
 
-    public void evictUserFromLoginAttemptCache(String username) {
-        loginAttemptCache.invalidate(username);
-    }
-    
+	}
+
+	public boolean hasExceededMaxAttempts(String username) {
+		try {
+			return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS; // Echec de connexion
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+		return false; // Connexion OK
+	}
+
+	public void evictUserFromLoginAttemptCache(String username) {
+		loginAttemptCache.invalidate(username);
+	}
 
 	public void loginFailed(final String key) {
-		
+
 		int attempts;
 		try {
-			
+
 			attempts = loginAttemptCache.get(key);
-			
+
 		} catch (final ExecutionException e) {
-			
+
 			attempts = 0;
 		}
-		
-		attempts ++;
+
+		attempts++;
 		loginAttemptCache.put(key, attempts);
-		
+
 	}
-	
+
 	public boolean isBlocked() {
-		
+
 		try {
 			return loginAttemptCache.get(getClientIP()) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
-			
+
 		} catch (final ExecutionException e) {
 
 			return false;
 		}
-		
+
 	}
-	
+
 	private String getClientIP() {
-		
+
 		final String xfHeader = request.getHeader("X-Forwarded-For");
-		
-		if(xfHeader != null) {
-			
+
+		if (xfHeader != null) {
+
 			return xfHeader.split(",")[0];
-			
+
 		}
 		return request.getRemoteAddr();
 	}
